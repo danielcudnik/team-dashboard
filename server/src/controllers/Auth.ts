f17a6988e3d780a86fb66b60e6ed4f4ce73777d9
danielcudnik <cudnikdaniel1993@gmail.com>
2019-12-01 17:50:37 +0100

    add validation middleware,  add exceptions folder and first DTO

--- server/src/controllers/Auth.ts
+++ server/src/controllers/Auth.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'express';
-import { Controller, Get, Post, BodyValidator } from './decorators';
+import { Controller, Get, Post, BodyValidator, Use } from './decorators';
 import { hash } from 'bcrypt';
 
 import { User } from '../database/models/User';
+import { validationMiddleware } from '../middleware/validation.middleware';
+import { CreateUserDto } from '../dtos/User/CreateUserDto';
 
 @Controller('/auth')
 class AuthController {
@@ -13,8 +15,9 @@ class AuthController {
     }
 
     @Post('/signup')
+    @Use(validationMiddleware(CreateUserDto))
     async signup(req: Request, res: Response): Promise<void> {
-        const { username, password, email } = req.body;
+        const { username, password, email, repeatedPassword } = req.body;
         const hashedPassword = await hash(password, 12);
 
         const user = await User.create({ username, password: hashedPassword });
